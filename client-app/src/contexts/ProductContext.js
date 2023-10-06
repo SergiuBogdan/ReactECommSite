4735386ceed3cb80e95226ed66b81bf78988a65c
Sergiu <sergiu.bogdan09@yahoo.com>
2023-10-06 20:58:49 +0300

    Added React Query for fetching data

--- client-app/src/contexts/ProductContext.js
+++ client-app/src/contexts/ProductContext.js
@@ -1,5 +1,6 @@
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 // Creem context
 export const ProductContext = createContext();
 
@@ -7,12 +8,23 @@ const ProductProvider = ({ children }) => {
   // State-ul produselor, array
   const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    axios.get("http://localhost:5000/api/activities").then((res) => {
-      console.log(res.data);
-      setProducts(res.data);
-    });
-  }, []);
+  //React Query mode of fetching data.
+  useQuery({
+    queryKey: ["beers"],
+    queryFn: async () => {
+      const response = await axios.get("http://localhost:5000/api/activities");
+      setProducts(response.data);
+      return response.data;
+    },
+  });
+
+  //old way of fetching data
+  // useEffect(() => {
+  //   axios.get("http://localhost:5000/api/activities").then((res) => {
+  //     console.log(res.data);
+  //     setProducts(res.data);
+  //   });
+  // }, []);
 
   return (
     <ProductContext.Provider value={{ products }}>
--- client-app/src/index.js
+++ client-app/src/index.js
@@ -5,16 +5,29 @@ import App from "./App";
 import ProductProvider from "./contexts/ProductContext";
 import SidebarProvider from "./contexts/SidebarContext";
 import CartProvider from "./contexts/CartContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000, //Time that our cache data will stay on, before making a new request
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <SidebarProvider>
-    <CartProvider>
-      <ProductProvider>
-        <React.StrictMode>
-          <App />
-        </React.StrictMode>
-      </ProductProvider>
-    </CartProvider>
-  </SidebarProvider>
+  <QueryClientProvider client={queryClient}>
+    <SidebarProvider>
+      <CartProvider>
+        <ProductProvider>
+          <React.StrictMode>
+            <App />
+            <ReactQueryDevtools />
+          </React.StrictMode>
+        </ProductProvider>
+      </CartProvider>
+    </SidebarProvider>
+  </QueryClientProvider>
 );
